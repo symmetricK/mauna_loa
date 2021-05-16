5980a7c16e554182f55065de66b5047a92805ce7
Kei Horikawa <horikkei@oregonstate.edu>
2021-05-16 22:04:56 +0000

    update ml.py

--- ml.py
+++ ml.py
@@ -7,13 +7,14 @@ def get_df(filename):
     INPUT:filename e.g. mauna_loa.csv
     OUTPUT: Pandas Dataframe
     '''
-    ...
-   
+    dataframe = pd.read_csv(filename)
+    return dataframe
 
 def plot_df(df):
     '''
     INPUT: Pandas DataFrame
     OUTPUT: handle to plot axis
     '''
-    
+    df_plot=plt.plot(df[0],df[1])
+    return df_plot
     
