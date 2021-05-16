35b4791886dda9d0d8e00af57a688fb322f8145e
Kei Horikawa <horikkei@oregonstate.edu>
2021-05-16 21:39:13 +0000

    Add initial files

---
+++ .ipynb_checkpoints/ml-checkpoint.py
@@ -0,0 +1,19 @@
+##This file contains functions that plots mauna_loa carbon dioxide data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+def get_df(filename):
+    '''
+    INPUT:filename e.g. mauna_loa.csv
+    OUTPUT: Pandas Dataframe
+    '''
+    ...
+   
+
+def plot_df(df):
+    '''
+    INPUT: Pandas DataFrame
+    OUTPUT: handle to plot axis
+    '''
+    
+    
